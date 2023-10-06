0b92f2ab4588b42e1fd0ba947217df37c403c47a
shira <93284370+rxshira@users.noreply.github.com>
2023-10-06 12:47:30 -0700

    Update beamChallenge.py

--- beamChallenge.py
+++ beamChallenge.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 pi = np.pi
 
-NPRO_wavelength = 1064e-9 #meters
+from physunits import *
+
+NPRO_wavelength = 1064*nm
 
 def gaussian_beam(z, z0, w0):
     # Function shift
@@ -22,7 +24,7 @@ def gaussian_beam(z, z0, w0):
 z_arr = np.arange(start=-100, stop=100, step=5)
 print("this is beam waist in meters:")
 print("when z=", z_arr, ", z0=0.0m, w0=2mm")
-print(gaussian_beam(z_arr, z0=0.0, w0=2e-3))
+print(gaussian_beam(z_arr, z0=0.0, w0=2*mm))
 
 ### CHALLENGE ### 
 
@@ -32,8 +34,74 @@ print(gaussian_beam(z_arr, z0=0.0, w0=2e-3))
 
 ###Answer###
 
-plt.plot(z_arr, gaussian_beam(z_arr, z0=0.0, w0=2e-3), 'ro')
-plt.suptitle('Beam waist plot')
-plt.xlabel('z position (m)')
-plt.ylabel('Beam Radius (m)')
+def shade_gaussian(axis, z, waist_profile, color):
+    axis.plot(z, waist_profile, c=color)
+    axis.plot(z, -waist_profile, c=color)
+    axis.fill_between(z, waist_profile, -waist_profile, color=color, alpha=0.5)
+
+plt.figure()
+ax = plt.subplot(111)
+shade_gaussian(ax, z_arr, gaussian_beam(z_arr, z0=0.0, w0=2e-3)/mm, color='green')
+ax.set_title('Beam waist plot')
+ax.set_xlabel('z position (m)')
+ax.set_ylabel('Beam Radius (mm)')
+
+#plt.show()
+
+### Propagate gaussian beams using ray transfer matrices
+
+# Beam parameter is
+# q = z0 + 1j * zR
+
+# ABCD matrix transforms q as:
+# [[A, B], 
+#  [C, D]]
+
+# This is how a beam parameter transforms under an ABCD matrix
+# qout = (qin * A + B) / (qin * C + D)
+
+# For a series of ABCD matrices, we can combine the net effect as
+# Mpath = Mzlaser0 @ Mlens1 @ Mm1 @ Mlens2 ... @ METM
+
+def free_space(qin, d):
+
+    A = 1
+    B = d 
+    C = 0
+    D = 1
+
+    qout = (qin * A + B) / (qin * C + D)
+    return qout
+
+# HERE ADD thin_lens, refraction
+# TEST the following path example:
+# 
+# w(-z) = 2*cm------(f=100mm)------30*cm------(f=200mm)--------w(z=?)
+# 
+# What happened?
+
+# Test
+w0 = 2*mm
+k0 = 2 * np.pi * 1 / NPRO_wavelength
+zR0 = 0.5 * k0 * w0**2
+qinit = 0*m + 1j * zR0
+
+# As a function of z_arr, propagate and plot! 
+qout = []
+for z_pos in z_arr:
+    qout.append(free_space(qinit, z_pos))
+qout = np.array(qout)
+
+plt.figure()
+ax = plt.subplot(111)
+shade_gaussian(ax, z_arr, w0*np.sqrt(1 + (qout.real/qout.imag)**2), color='goldenrod')
 plt.show()
+
+# Thought provoking ideas: Challenge
+
+# q = (z-z0) + 1j * (0.5 * k * w0**2)
+
+# q.real = z - z0
+# q.imag = (0.5 * k * w0**2)
+
+# wz = np.sqrt(q.imag / ( np.pi / NPRO_wavelength)) * np.sqrt(1 + (q.real/q.imag)**2)
